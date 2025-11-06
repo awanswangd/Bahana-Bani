b5cf2e4d3def44ae08c1d4efb6e0422a293a53cf
jordyakbar1 <jordiakbar80@gmail.com>
2025-11-06 22:58:13 +0700

    mekanik awal dan game loop

---
+++ mekanik-awal/enemy.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var blue: Color = Color("#4682b4")
+@export var green: Color = Color("#639765")
+@export var red: Color = Color("#a65455")
+
+@export var speed: float = 0.5
+
+
+
+@onready var prompt = $RichTextLabel
+
+func _physics_process(delta: float) -> void:
+	global_position.x -= speed
+
+func get_prompt() -> String:
+	return prompt.text
+
+func set_next_character(next_character_index: int):
+	var blue_text = get_bbcode_color_tag(blue) + prompt.text.substr(0, next_character_index) + get_bbcode_end_color_tag()
+	var green_text = get_bbcode_color_tag(green) + prompt.text.substr(next_character_index,1 ) + get_bbcode_end_color_tag()
+	var red_text = ""
+	
+	if next_character_index != prompt.text.length():
+		red_text = get_bbcode_color_tag(red) + prompt.text.substr(next_character_index + 1, prompt.text.length() - next_character_index + 1) + get_bbcode_end_color_tag()
+
+	prompt.parse_bbcode(blue_text + green_text + red_text)
+
+func get_bbcode_color_tag(color: Color) -> String:
+	return "[color=#" + color.to_html(false) + "]"
+
+func get_bbcode_end_color_tag() -> String:
+	return "[/color]"
---
+++ mekanik-awal/main.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+var Enemy = preload("res://enemy.tscn")
+
+@onready var spawn_timer = $SpawnTimer
+@onready var enemy_container = $enemycontainer
+@onready var spawn_container = $SpawnContainer
+var active_enemy = null
+var current_letter_index: int = -1
+
+func _ready() -> void:
+	randomize()
+	spawn_timer.start()
+	spawn_enemy()
+
+
+func find_new_active_enemy(typed_character: String):
+	print("Searching for enemy starting with: '%s'" % typed_character)
+	for enemy in enemy_container.get_children():
+		var prompt = enemy.get_prompt()
+		print("Checking enemy with prompt: '%s'" % prompt)
+		var next_character = prompt.substr(0, 1).to_lower()
+		if next_character == typed_character:
+			print("found new enemy that start with %s" % next_character)
+			active_enemy = enemy
+			current_letter_index = 1
+			active_enemy.set_next_character(current_letter_index)
+			return
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and not event.is_pressed():
+		var key_typed = event.as_text().to_lower()
+		if key_typed == "":
+			return
+		
+		print("Input detected: '%s'" % key_typed)
+		
+		if active_enemy == null:
+			find_new_active_enemy(key_typed)
+		else:
+			var prompt = active_enemy.get_prompt()
+			var next_character = prompt.substr(current_letter_index, 1).to_lower()
+			if key_typed == next_character:
+				print("success %s" % key_typed)
+				current_letter_index += 1
+				active_enemy.set_next_character(current_letter_index)
+				if current_letter_index == prompt.length():
+					print("done")
+					current_letter_index = -1
+					active_enemy.queue_free()
+					active_enemy = null
+			else:
+				print("gagal %s harusnya %s" % [key_typed, next_character])
+
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_enemy()
+
+func spawn_enemy():
+	var enemy_instance = Enemy.instantiate()
+	var spawns = spawn_container.get_children()
+	var index = randi() % spawns.size()
+	enemy_container.add_child(enemy_instance)
+	enemy_instance.global_position = spawns[index].global_position
